9e13e3c56aa6a527006287dddb7b86b3e9b899c2
Paul O'Leary McCann <polm@dampfkraft.com>
2021-04-19 21:57:22 +0900

    Retain formatting in demo
    This also adds a radio box for the romaji system.

--- demo.py
+++ demo.py
@@ -3,26 +3,37 @@ from cutlet import Cutlet
 import pysbd 
 
 senter = pysbd.Segmenter(language="ja", clean=False)
+ZKS = "　" # full width space
 
-def romajify(sents, system="hepburn"):
+def romajify(text, system="hepburn"):
+    out = ""
     katsu = Cutlet(system)
-    return ' '.join([katsu.romaji(text) for text in sents])
+    for line in text.split("\n"):
+        for chunk in line.split(ZKS):
+            for sent in senter.segment(chunk):
+                out += katsu.romaji(sent) + " "
+            out += ZKS
+        out += "\n"
+
+    return out
 
 st.set_page_config("cutlet ローマ字変換ツール", 'https://cotonoha.io/android-icon-144x144.png')
 
 st.title("cutlet ローマ字変換")
 
+system = st.radio(
+        "ローマ字の種類",
+        ("ヘボン式", "訓令式"))
+
 text = st.text_area('変換したいテキストを入力してください', 
         "吾輩は猫である。名前はまだ無い。")
 
-sents = senter.segment(text)
 
-"### ヘボン式"
+systems = {"ヘボン式": "hepburn", "訓令式": "kunrei"}
+system = systems[system]
 
-st.write(romajify(sents, 'hepburn'))
+"# 変換結果"
 
-"### 訓令式"
-
-st.write(romajify(sents, 'kunrei'))
+st.write(romajify(text, system))
 
 st.markdown('<div><a style="width: 200px;margin: 0 auto; display: block" href="https://cotonoha.io"><img src="https://cotonoha.io/cotonoha.png" /></a></div>', unsafe_allow_html=True)
