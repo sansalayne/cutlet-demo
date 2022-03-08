686fa9871cd65e75823e84e0f4d422c0163cc42d
sansalayne <51176187+sansalayne@users.noreply.github.com>
2022-03-08 12:02:13 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -34,6 +34,9 @@ system = systems[system]
 
 "# 変換結果"
 
-st.write(romajify(text 1 \n text 2 \n text 3, system))
+st.write(romajify(text, system))
+st.write(romajify(text, system))
+st.write(romajify(text, system))
+
 
 st.markdown('<div><a style="width: 200px;margin: 0 auto; display: block" href="https://cotonoha.io"><img src="https://cotonoha.io/cotonoha.png" /></a></div>', unsafe_allow_html=True)
